287d1244a5e17aaf8447e6398f8d88b8236ff37f
acediat <128941974+acediat@users.noreply.github.com>
2023-10-14 19:44:26 +0300

    some changes

--- app/src/main/java/ru/acediat/simplestory/GameRenderView.kt
+++ app/src/main/java/ru/acediat/simplestory/GameRenderView.kt
@@ -1,26 +1,39 @@
 package ru.acediat.simplestory
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.SurfaceView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import ru.acediat.engine.ScreenHolder
+import java.util.concurrent.atomic.AtomicBoolean
 
+@SuppressLint("ViewConstructor")
 class GameRenderView(
     context: Context,
     private val screenHolder: ScreenHolder,
 ) : SurfaceView(context) {
 
-    private var running = false
+    private var running = AtomicBoolean(false)
 
     private val scope = CoroutineScope(Dispatchers.Default)
+    private var renderJob: Job? = null
 
-    fun resume() = scope.launch { render() }
+    fun resume() = scope.launch {
+        render()
+    }
 
     private fun render() {
-        while(running) {
-            if ()
+        while(running.get()) {
+
         }
+    }
+
+    private fun stop() {
+        running.compareAndSet(true, false)
+        renderJob?.cancel()
+        renderJob = null
     }
 }
--- core/engine/src/main/java/ru/acediat/engine/Screen.kt
+++ core/engine/src/main/java/ru/acediat/engine/Screen.kt
@@ -1,4 +1,10 @@
 package ru.acediat.engine
 
+interface ScreenState
+
+interface ScreenEvent
+
 abstract class Screen {
+
+    abstract fun update(deltaTime: Double)
 }
